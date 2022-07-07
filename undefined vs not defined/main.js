86bc483a658ac5310de5ee791b320fb5f3c8e224
Ravi <rkm4198@gmail.com>
2022-07-07 18:43:38 +0530

    undefined vs not defined

---
+++ undefined vs not defined/main.js
@@ -0,0 +1,11 @@
+// undefined is a placeholder value for the variable
+// not defined : if we want to access variable which is not declared then reference error shows 'not defined'
+
+var a;
+console.log(a); // undefined
+
+if (a === undefined) {
+    console.log('a is undefined');
+} else {
+    console.log('a is not defined');
+}
