9478e5700a9282a28a2a7018090d378ba7585b4c
Ravi <rkm4198@gmail.com>
2022-07-07 17:17:22 +0530

    Hoisting

---
+++ Hoisting/main.js
@@ -0,0 +1,26 @@
+var x = 7;  // before valueassignment javascript has reserverd the memory and has predefined value is undefined
+
+function getName() {
+    console.log("Namaste in JavaScript");
+}
+
+getName();
+console.log(x);
+
+// Access Before initialization or assign value to variable
+// In most programming it will cause error
+// Javascript has hoisting to deal with it.
+
+// getName();
+// console.log(x);    // undefined
+// console.log(getName);
+
+// var x = 7;
+
+// function getName() {
+//     console.log("Namaste JavaScript");
+// }
+
+// undefined and not defined are not same in javascript
+//
+
