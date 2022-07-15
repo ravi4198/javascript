db81f9df4c0024153f8b102c7cdee4d0098a6d1f
Ravi <rkm4198@gmail.com>
2022-07-15 11:39:59 +0530

    multiplication

---
+++ nestedloop/main.js
@@ -0,0 +1,6 @@
+// Multiplication of number two number
+for (let firstNum = 0; firstNum < 2; firstNum++) {
+    for (let secondNum = 0; secondNum <= 9; secondNum++) {
+        console.log(firstNum,"times", secondNum, "is equals to", firstNum * secondNum)
+    }
+}
